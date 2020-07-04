af152ff7df347fe6917af003cad5b23f045f92d2
Roger Lam <rogerlam@wylco.org>
2020-07-04 11:20:27 -0400

    add basic routes

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ express()
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
+
+  .get('/slingair/flights', getFlightNums)
+  .get('/slingair/flights/:flight', returnFlightInfo)
+  .get('/slingair/users', getAllUsers) //Accepts query params of `limit` and `start` for pagination. _If values are not provided, it will return the first 10 users_
+  .post('/slingair/users', addUser)
   .get('/flights/:flightNumber', handleFlight)
   .use((req, res) => res.send('Not Found'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
